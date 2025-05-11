4453ca0889e781bdfd3cebcacea8da04b724e8c8
Shikha <150659967+SiyaShikha@users.noreply.github.com>
2025-05-12 02:32:19 +0530

    Implemented PrintWriter to write output to a file

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,31 @@
 import Rover.Rover;
-import Rover.Movements;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.io.PrintWriter;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         File inputFile = new File("io/input.txt");
         Scanner scanner = new Scanner(inputFile);
+        PrintWriter writer = new PrintWriter("io/output.txt");
         List<Rover> rovers = new ArrayList<>();
 
+        populateRoversFromFile(scanner, rovers);
+        List<String> finalPositions = getFinalPositions(rovers);
+        saveFinalPositionsToFile(writer, finalPositions);
+
+        scanner.close();
+        writer.close();
+
+        System.out.println("Output is written to file output.txt");
+    }
+
+    private static void populateRoversFromFile(Scanner scanner, List<Rover> rovers) {
         while (scanner.hasNextLine()) {
             String[] position = scanner.nextLine().split(" ");
             int x = Integer.parseInt(position[0]);
@@ -23,13 +35,13 @@ public class Main {
             String commands = scanner.nextLine();
             rovers.add(new Rover(x, y, heading, commands));
         }
+    }
 
-        List<String> finalPositions = getFinalPositions(rovers);
-
-        System.out.println("Final positions of the rovers:");
-        finalPositions.forEach(System.out::println);
-
-        scanner.close();
+    private static void saveFinalPositionsToFile(PrintWriter writer, List<String> finalPositions) {
+        writer.println("Final positions of the rovers:");
+        for (String finalPosition : finalPositions) {
+            writer.println(finalPosition);
+        }
     }
 
     private static List<String> getFinalPositions(List<Rover> rovers) {
